26d6f56451ee8b181849cdedc793a56135be6e47
Eric Ramat <eric.ramat@gmail.com>
2017-09-08 15:58:53 +0200

    TP2: q1.2

--- src/test/CharacterTest.java
+++ src/test/CharacterTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import com.sun.istack.internal.Nullable;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -7,6 +8,8 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -67,4 +70,131 @@ public class CharacterTest {
             Assert.fail("should have a class called Monster in package lsg.charaters");
         }
     }
+
+    @Test
+    public void testAttributes() {
+        try {
+            Class <?> c = Class.forName("lsg.characters.Character");
+            Field f1 = c.getDeclaredField("name");
+            Field f2 = c.getDeclaredField("life");
+            Field f3 = c.getDeclaredField("maxLife");
+            Field f4 = c.getDeclaredField("stamina");
+            Field f5 = c.getDeclaredField("maxStamina");
+
+        } catch (ClassNotFoundException e) {
+            Assert.fail("should have a class called Monster");
+        } catch (NoSuchFieldException e) {
+            Assert.fail("should have some missed attributes (name, life, maxLife, stamina or maxStamina)");
+        }
+    }
+
+    @Nullable
+    private Constructor<?> searchDefaultConstructor(Class<?> c) {
+        for (Constructor<?> constructor : c.getConstructors()) {
+            if (constructor.getParameterCount() == 0) {
+                return constructor;
+            }
+        }
+        return null;
+    }
+
+    @Test
+    public void testCharacterToString() {
+        Class<?> c = null;
+        try {
+            c = Class.forName("lsg.characters.Character");
+            Method ts = c.getDeclaredMethod("toString");
+        } catch (ClassNotFoundException e) {
+            Assert.fail("should have a class called lsg.characters.Character");
+        } catch (NoSuchMethodException e) {
+            Assert.fail("should have a method called toString");
+        }
+    }
+
+    @Test
+    public void existMonsterToString() {
+        Class<?> c = null;
+        try {
+            c = Class.forName("lsg.characters.Monster");
+            Method ts = c.getDeclaredMethod("toString");
+
+            Assert.fail("should have not a method called toString in Monster class");
+        } catch (ClassNotFoundException e) {
+            Assert.fail("should have a class called lsg.characters.Monster");
+        } catch (NoSuchMethodException e) {
+        }
+    }
+
+    @Test
+    public void existHeroToString() {
+        Class<?> c = null;
+        try {
+            c = Class.forName("lsg.characters.Hero");
+            Method ts = c.getDeclaredMethod("toString");
+
+            Assert.fail("should have not a method called toString in Hero class");
+        } catch (ClassNotFoundException e) {
+            Assert.fail("should have a class called lsg.characters.Hero");
+        } catch (NoSuchMethodException e) {
+        }
+    }
+
+    @Test
+    public void testMonsterToString() {
+        Class<?> c = null;
+        try {
+            c = Class.forName("lsg.characters.Monster");
+            Constructor<?> constructor = searchDefaultConstructor(c);
+            Object o = constructor.newInstance();
+            Method ts = c.getMethod("toString");
+
+            try {
+                Field f = c.getDeclaredField("INSTANCES_COUNT");
+                String str = ((String)(ts.invoke(o))).replaceAll("Monster_[0-9]\t", "");
+
+                Assert.assertEquals("[ Monster ]\tLIFE: 10\tSTAMINA: 10\t(ALIVE)", str);
+            } catch (NoSuchFieldException e) {
+                Assert.assertEquals("[ Monster ]\tMonster\tLIFE: 10\tSTAMINA: 10\t(ALIVE)", (String)(ts.invoke(o)));
+            }
+        } catch (ClassNotFoundException e) {
+            Assert.fail("should have a class called lsg.characters.Monster");
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testHeroToString() {
+        try {
+            Class<?> c = Class.forName("lsg.characters.Hero");
+            Constructor<?> constructor = searchDefaultConstructor(c);
+            Object o = constructor.newInstance();
+            Method ts = c.getMethod("toString");
+
+            try {
+                Method m = c.getMethod("isAlive");
+
+                Assert.assertEquals("[ Hero ]\tGregooninator\tLIFE: 100\tSTAMINA: 50\t(ALIVE)", (String)(ts.invoke(o)));
+            } catch (NoSuchMethodException e) {
+                Assert.assertEquals("[ Hero ]\tGregooninator\tLIFE: 100\tSTAMINA: 50", (String)(ts.invoke(o)));
+            }
+        } catch (ClassNotFoundException e) {
+            Assert.fail("should have a class called lsg.characters.Hero");
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
